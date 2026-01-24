07c3895b2c96c56229c26cb889417d8a111148db
BrianKLeman <brian.leman@hotmail.co.uk>
2026-01-24 12:47:46 +0000

    PrescriptionsTests Progress

---
+++ ServicesLayer.Tests/PrescriptionsTests.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer;
+using ServicesLayer.Services;
+namespace ServicesLayer.Tests
+{
+    public class PrescriptionsTests
+    {
+        private IPrescriptionDataAccess Prescriptions;
+        [SetUp]
+        public void Setup()
+        {
+            //Prescriptions = new ServicesLayer.Services.Prescriptions();
+            // I need to start Using an InMemory representation of the database for testing
+            // the service layer.
+
+        }
+
+        [Test]
+        public void InvalidPersonCodeReturnsEmptyArray()
+        {
+            Assert.Pass("Testing github passing test execution.");
+        }
+    }
+}
